6632a5afbc917e7645939902576c23363a45c349
Shadow King <105120735+MasterShad0w@users.noreply.github.com>
2022-07-10 02:27:38 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/MasterShad0w/IMDb-Movie-Bot.git /IMDb-Movie-Bot
-cd /IMDb-Movie-Bot
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 bot.py
